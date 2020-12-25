0a9f066c60fda4d803bec9599f3d02ec4f0b0112
Blackmanx <blackman@sajl.tk>
2020-12-25 02:50:30 +0100

    Fixes

--- ft_printf/libft/ft_strnew.c
+++ ft_printf/libft/ft_strnew.c
@@ -6,13 +6,23 @@
 /*   By: prodrigo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 02:00:48 by prodrigo          #+#    #+#             */
-/*   Updated: 2020/12/25 02:22:05 by prodrigo         ###   ########.fr       */
+/*   Updated: 2020/12/25 02:49:47 by prodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnew(size_t size)
+static void	*ft_memalloc(size_t size)
+{
+	void *str;
+
+	if (!(str = (void *)malloc(sizeof(*str) * size)))
+		return (NULL);
+	ft_bzero(str, size);
+	return (str);
+}
+
+char		*ft_strnew(size_t size)
 {
 	char *str;
 
