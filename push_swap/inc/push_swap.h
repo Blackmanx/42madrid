ccb6b0a96590ed67cebf3f16073d8f2625ed36a5
Pedro manuel Rodrigo manzanares <prodrigo@c1r16s1.42madrid.com>
2021-08-04 19:16:38 +0200

    Change interface, organize files

--- push_swap/inc/push_swap.h
+++ push_swap/inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: prodrigo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 17:40:51 by prodrigo          #+#    #+#             */
-/*   Updated: 2021/06/22 19:16:36 by prodrigo         ###   ########.fr       */
+/*   Updated: 2021/08/04 19:15:38 by prodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,33 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef struct s_node
-{
-	int				data;
-	struct s_node	*next;
-}	t_node;
-
 typedef struct s_stack
 {
-	struct s_node	*head;
+	int				data;
+	struct s_stack	*next;
+	struct s_stack	*prev;
 }	t_stack;
+
+typedef struct s_table
+{
+	int			*sorted;
+	int			len;
+	int			aux;
+	int			temp;
+	t_stack		*stack_a;
+	t_stack		*stack_b;
+}	t_table;
 
 int		sa(t_stack *a);
 int		sb(t_stack *b);
-int		ss(t_stack *a, t_stack *b);
-int		pa(t_stack *a, t_stack *b);
-int		pb(t_stack *a, t_stack *b);
+int		ss(t_table *table);
+int		pa(t_table *table);
+int		pb(t_table *table);
 int		ra(t_stack *a);
 int		rb(t_stack *b);
-int		rr(t_stack *a, t_stack *b);
+int		rr(t_table *table);
 int		rra(t_stack *a);
 int		rrb(t_stack *b);
-int		rrr(t_stack *a, t_stack *b);
+int		rrr(t_table *table);
 
 #endif
