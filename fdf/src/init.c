a1f69fe791deece0bb106076ccd0e5b3a7dc457d
Blackmanx <blackman@sajl.tk>
2023-03-23 18:08:26 +0100

    [TEST]: fdf: init: Let's fiddle around with random values to understand how mlx pixel put in this lib acts

--- fdf/src/init.c
+++ fdf/src/init.c
@@ -6,7 +6,7 @@
 /*   By: prodrigo <prodrigo@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 17:24:27 by prodrigo          #+#    #+#             */
-/*   Updated: 2023/03/13 21:58:31 by prodrigo         ###   ########.fr       */
+/*   Updated: 2023/03/23 18:04:16 by prodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,23 @@ void	init_fdf(char **argv, t_fdf *fdf)
 	int	x;
 	int	y;
 
-	x = -1;
-	y = -1;
+	x = 0;
+	y = 0;
 	argv = NULL;
 	fdf->lib.mlx = mlx_init();
-	fdf->lib.win = mlx_new_window(fdf->lib.mlx, 1920, 1080, "Amog Us 2!");
-	fdf->img.img = mlx_new_image(fdf->lib.mlx, 1920, 1080);
+	fdf->lib.win = mlx_new_window(fdf->lib.mlx,
+			WINDOW_W, WINDOW_H, "Amog Us 2!");
+	fdf->img.img = mlx_new_image(fdf->lib.mlx, WINDOW_W, WINDOW_H);
 	fdf->img.addr = mlx_get_data_addr(fdf->img.img, &fdf->img.bpp,
 			&fdf->img.len, &fdf->img.endian);
-	while (++x < 1920)
+	while (x < WINDOW_W)
 	{
-		while (++y < 1080)
-			my_mlx_pixel_put(fdf, x, y, 0x00FF0000);
+		while (y < WINDOW_H)
+		{
+			my_mlx_pixel_put(fdf, x, y, WHITE);
+			y += rand() % 30;
+		}
+		x += rand() % 50;
 		y = 0;
 	}
 	mlx_put_image_to_window(fdf->lib.mlx, fdf->lib.win, fdf->img.img, 0, 0);
