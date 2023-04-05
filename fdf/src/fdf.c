0766882b2a46e10037f487dddc71fa8489172e70
Blackmanx <blackman@sajl.tk>
2023-04-05 17:45:09 +0200

    fdf: Add initial map reading function

--- fdf/src/fdf.c
+++ fdf/src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: prodrigo <prodrigo@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 13:12:53 by prodrigo          #+#    #+#             */
-/*   Updated: 2023/04/05 12:30:40 by prodrigo         ###   ########.fr       */
+/*   Updated: 2023/04/05 17:05:30 by prodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ int	main(int argc, char *argv[])
 		exit_error("Error: Invalid arguments", INVALID_ARGS);
 	check_type(argv[1]);
 	init_fdf(&fdf);
+	read_map(&fdf, argv[1]);
 	mlx_hook(fdf.lib.win, 2, 1, key_press, &fdf);
 	mlx_hook(fdf.lib.win, 17, (1U << 17), close_window, &fdf);
 	mlx_loop(fdf.lib.mlx);
--- fdf/src/parser.c
+++ fdf/src/parser.c
@@ -6,16 +6,44 @@
 /*   By: prodrigo <prodrigo@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 20:50:26 by prodrigo          #+#    #+#             */
-/*   Updated: 2023/04/05 12:11:31 by prodrigo         ###   ########.fr       */
+/*   Updated: 2023/04/05 17:38:04 by prodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
 
+static void	fill_map(char *line, int *map, t_fdf *fdf)
+{
+	int		i;
+
+	i = 0;
+	ft_split(line, ' ', fdf);
+	while (fdf->read.buf[i])
+	{
+		map[i] = ft_atoi(fdf->read.buf[i]);
+		i++;
+	}
+	free(fdf->read.buf);
+}
 
 int	read_map(t_fdf *fdf, char *argv)
 {
-	fdf = NULL;
-	argv = NULL;
+	int	i;
+	int	fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		exit_error("Fdf file could not be opened\n", FILE_OPEN);
+	fdf->map = (int **)malloc(sizeof(int *) * fdf->rows);
+	i = -1;
+	while (++i < fdf->rows)
+		fdf->map[i] = (int *)malloc(sizeof(int) * fdf->cols);
+	i = 0;
+	while (get_next_line(fd, &map->line, &map->read.b, &map->read.l) > 0)
+	{
+		fill_map(fdf->line, fdf->map[i++], fdf);
+		free(fdf->line);
+		fdf->line = NULL;
+	}
 	return (0);
 }
